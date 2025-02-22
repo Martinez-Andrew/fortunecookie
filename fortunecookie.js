7fd253ba486c1746ccbcc7eb56bbae88f26353b1
Andrew Martinez <james99andrew@gmail.com>
2025-02-21 21:01:02 -0600

    added random fortune function

--- fortunecookie.js
+++ fortunecookie.js
@@ -1,1 +1,14 @@
-console.log('This is the birth of fortunecookie')
+let who = ['Your dog', 'My grandma', 'Your turtle', 'Your bird', 'You', 'Your mother', 'Your father', 'Your cat', 'Your fish'];
+let fortune = ['be a millionaire', 'find true love', 'go to Japan', 'have a boy', 'have a girl', 'have a great day', 'travel the world', 'get their dream job', 'eat an ice cream', 'read a fantastic book'];
+let when = ['today', 'tomorrow', 'next week', 'next month', 'next year', 'in 10 years'];
+
+//Returns random position within array
+function getRandomPosition(who) {
+    if (!Array.isArray(who) || who.length === 0) {
+        console.log("Invalid array: must be a non-empty array.");
+    }
+    return Math.floor(Math.random() * who.length);
+}
+
+const randomIndex = getRandomPosition(who);
+console.log(`${who[randomIndex]} will ${fortune[getRandomPosition(fortune)]} ${when[getRandomPosition(when)]}.`);
